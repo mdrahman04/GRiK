1253920cc4d180d534d36db337b9ba975427a004
rahmanx1 <mdmahirx1@gmail.com>
2023-08-01 11:31:50 +0600

    third commit

--- paid/script.js
+++ paid/script.js
@@ -1,3 +1,18 @@
+//overlay
+const checkbox = document.getElementById("check");
+const overlay = document.querySelector(".overlay");
+
+checkbox.addEventListener("change", function () {
+    if (this.checked) {
+        document.body.style.overflow = "hidden"; // Disable scrolling
+        overlay.style.display = "block"; // Show the overlay
+    } else {
+        document.body.style.overflow = ""; // Enable scrolling
+        overlay.style.display = "none"; // Hide the overlay
+    }
+});
+//end
+
 document.addEventListener("DOMContentLoaded", function () {
   const searchInput = document.getElementById("search-input");
   const filterButton = document.getElementById("filter-button");
